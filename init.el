63e927eab4b7f996fc8690c52b52328dca9b9525
Eddie Hillenbrand <eddie.hillenbrand@adcolony.com>
2015-12-02 20:15:59 -0800

    Initialization file and core functionality

--- init.el
+++ init.el
@@ -1,1 +1,13 @@
 ;; Eddie's Initialization File
+
+(message "Loading Eddie's initialization…")
+
+(setq eddie/dir
+      (file-name-directory (or load-file-name (buffer-file-name))))
+
+(defun eddie/load-file (file-name)
+  "Load a .org file."
+  (org-babel-load-file (expand-file-name (concat file-name ".org") eddie/dir)))
+
+(eddie/load-file "README")
+

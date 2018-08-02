1deb27cd8b7afb428c88c875e1643033b94483e2
Eddie Hillenbrand <eddie.hillenbrand@adcolony.com>
2018-08-02 16:02:53 -0700

    Setup LSP for Python

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (neotree projectile editorconfig zenburn-theme use-package project-explorer markdown-mode ido-vertical-mode groovy-mode gradle-mode google-c-style go-mode gitignore-mode cmake-mode))))
+    (company-lsp lsp-ui lsp-mode neotree projectile editorconfig zenburn-theme use-package project-explorer markdown-mode ido-vertical-mode groovy-mode gradle-mode google-c-style go-mode gitignore-mode cmake-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
